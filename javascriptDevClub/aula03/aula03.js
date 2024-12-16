6222195c67d555ff471dd22768865462cb931ceb
Juliano Correa <juliano.correa@sou.unifeob.edu.br>
2024-12-16 20:14:05 -0300

    Envio de pasta
    Envio de pasta aula03.

---
+++ javascriptDevClub/aula03/aula03.js
@@ -0,0 +1,24 @@
+/*
+
+If & Else -> Controlador de Fluxo
+
+If (SE)
+Else (SE NÃO)
+
+Operadores de Comparação
+> naior que
+< menor que
+== verifica se valor é igual
+=== verifica se tipo e valor são iguais
+>= maior ou igual a
+<= menor ou igual a
+
+*/
+
+const notaDoAluno = 6
+
+if(notaDoAluno > 5){
+    console.log("Aluno APROVADO")
+} else {
+    console.log("Aluno REPROVADO")
+}
