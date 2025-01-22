5be8c40c7a5a90f037b5b7d59e181a703b43e49c
Juliano Correa <juliano.correa@sou.unifeob.edu.br>
2025-01-22 19:43:30 -0300

    Envio de arquivo
    Envio do arquivo listas.py.

---
+++ ifsul/listas.py
@@ -0,0 +1,13 @@
+# exemplos sobre listas
+
+l1 = [2, 6, 8, 0, 35]
+# l1 [0, 1, 2, 3, 4]
+
+l2 = ['f', 0, 1, 3.14, 'abacaxi']
+
+print(l1[2])
+
+for i in range(len(l1)):
+    print(f'O {i + 1}º elemento da lista é {l1[i]}')
+
+print(l2)
