721eef416975d4574d09aa67d1c067ebdaffa65e
Juliano <72839366+julianocgc@users.noreply.github.com>
2025-07-13 16:12:00 -0300

    Atualização de arquivos
    Atualização de arquivos Flutter.

---
+++ javaTiagoAguiar/Arrays/src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        String[] names = new String[10];
+        System.out.println(Arrays.toString(names));
+    }
+
+    private static boolean toString(String[] names) {
+
+
+    }
+}
